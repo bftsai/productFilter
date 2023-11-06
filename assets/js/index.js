354980aa6443dafd9b15b12bf5402e57ab6ff557
bf_tsai <tsai19911026@gmail.com>
2023-11-06 13:52:42 +0800

    fix: 🐛 fix transformNum regular expression

--- assets/js/index.js
+++ assets/js/index.js
@@ -167,7 +167,7 @@ component.init(component.data);
 function transformNumber(num){
     let comma=/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g;
     num=String(num).replace(comma, ',');
-    return `${num}`;
+    return num;
 };
 //bootstrap
 (() => {
