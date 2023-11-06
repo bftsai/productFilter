e2fdb7a2aded98fa367340e7c4e8a321c6866d24
bf_tsai <tsai19911026@gmail.com>
2023-11-07 00:17:40 +0800

    fix: 🐛 fix form sort added clean

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,6 +14,8 @@ let filterResultP=document.querySelector('.filterResultP');
 const component={
     data:[
         {
+            id: 0,
+            location_ID: 1,
             title: '綠島自由行套裝行程',
             picUrl: 'https://i.imgur.com/BrPYJ7a.png',
             location: '台北',
@@ -24,6 +26,8 @@ const component={
             content: '嚴選超高CP值綠島自由行套裝行程，多種綠島套裝組合，提供台東綠島來回船票、綠島環島機車、綠島民宿住宿，行程加贈『綠島浮潛體驗』以及『綠島生態導覽』，讓你用輕鬆的綠島套裝自由行，也能深度認識綠島在地文化。'
         },
         {
+            id: 1,
+            location_ID: 1,
             title: '清境高空觀景步道二日遊',
             picUrl: 'https://i.imgur.com/ibnCmoJ.png',
             location: '台北',
@@ -34,6 +38,8 @@ const component={
             content: '清境農場青青草原數十公頃碧草，餵食著數以百計的綿羊和牛群，中央山脈最高的北三段群峰形成一堵如帶的高牆，攔住清晨的薄霧山嵐，成就了從花蓮翻山而來的雲瀑在濁水溪谷積成雲海，這些景觀豐沛了清境觀景步道的風格，也涵養它無可取代的特色。'
         },
         {
+            id: 2,
+            location_ID: 2,
             title: '南庄度假村露營車二日遊',
             picUrl: 'https://i.imgur.com/SVMu64X.png',
             location: '台中',
@@ -44,6 +50,8 @@ const component={
             content: '南庄雲水豪華露營車，快來擁有最愜意的露營體驗吧！ 一泊一食，輕鬆享受露營車樂趣。 獨立衛浴與私人戶外露臺。 入住豪華露營車還能使用戶外SPA大眾湯，感受美人湯魅力。'
         },
         {
+            id: 3,
+            location_ID: 2,
             title: '山林悠遊雙人套票',
             picUrl: 'https://i.imgur.com/US7fmoK.png',
             location: '台中',
@@ -54,6 +62,8 @@ const component={
             content: '山林悠遊套票，結合南投清境高空步道、雙龍瀑布七彩吊橋、瑞龍瀑布園區之熱門景點，帶您飽覽南投瑰麗的自然環境，體驗變化無窮的地形景觀，喜歡挑戰高空的您一定不可錯過。 （含雙龍瀑布入場券 x2）'
         },
         {
+            id: 4,
+            location_ID: 2,
             title: '漁樂碼頭釣魚體驗套票',
             picUrl: 'https://i.imgur.com/L1XbQKx.png',
             location: '台中',
@@ -64,6 +74,8 @@ const component={
             content: '台中全新親子景點寶熊漁樂碼頭，為知名釣具公司「OKUMA」所創立的觀光工廠。一樓藍白希臘漁村風商店街免費參觀。二樓釣魚故事館則設立全台唯一虛擬釣場，透過導覽讓你知道如何釣魚、魚餌怎麼區分，寓教於樂的台中景點！'
         },
         {
+            id: 5,
+            location_ID: 3,
             title: '熊森公園親子二日遊套票',
             picUrl: 'https://i.imgur.com/8yucwVe.png',
             location: '高雄',
@@ -135,24 +147,40 @@ const component={
     },
     addData(){
         let obj={};
+        obj.id=this.data.length-1;
+        obj.location_ID=productSelect.selectedIndex;
         obj.title=productName.value;
         obj.picUrl=productPicUrl.value;
         obj.location=productSelect.value;
         obj.currency= 'TWD';
-        obj.price=productPrice.value;
-        obj.remain=productRemain.value;
-        obj.star=productStar.value;
+        obj.price=Number(productPrice.value);
+        obj.remain=Number(productRemain.value);
+        obj.star=Number(productStar.value);
         obj.content=productDescribe.value;
-        console.log(obj);
+        
         this.data.push(obj);
+        this.sortData();
+    },
+    sortData(){
+        this.data.sort((a,b)=>{
+          if(a.location_ID>b.location_ID){
+            return 1;
+          }else if(a.location_ID<b.location_ID){
+            return -1;
+          }
+          else{
+            return 0;
+          }
+        });
+        
         this.render(this.data);
     },
     filter(location){
         let filterArr=this.data.filter(item=>{
-            if(location!=='全部地區'&&location!=='地區搜尋'){
+            if(location!=='全部地區'){
                 return location===item.location;
             }else{
-                return item
+                return item;
             }
         });
         this.render(filterArr);
@@ -164,11 +192,15 @@ const component={
 
 component.init(component.data);
 
-function transformNumber(num){
-    let comma=new RegExp("\B(?<!\.\d*)(?=(\d{3})+(?!\d))","g");
-    num=String(num).replace(comma, ',');
-    return num;
-};
+/*
+  regular expression 待驗證(ios can't work)
+*/
+// function transformNumber(num){
+//     let comma=new RegExp("\B(?<!\.\d*)(?=(\d{3})+(?!\d))","g");
+//     num=String(num).replace(comma, ',');
+//     return num;
+// };
+
 //bootstrap
 (() => {
   'use strict'
@@ -177,7 +209,9 @@ function transformNumber(num){
   function checkStar(){
     if(Number(productStar.value)===0){
       document.querySelector('.productStar+.invalid-feedback').textContent=`請填寫套票星級`;
+      document.querySelector('.productStar+.invalid-feedback').setAttribute("style","display: block;width: 100%;margin-top: 0.25rem;font-size: 0.875em;color: var(--bs-form-invalid-color);");
       productStar.classList.add('is-invalid');
+      productStar.classList.remove('is-valid');
       productStar.setAttribute("style","border-color: var(--bs-form-invalid-border-color);padding-right: calc(1.5em + 0.75rem);background-image: url('https://i.imgur.com/sjBnbp4.png');background-repeat: no-repeat;background-position: right calc(0.375em + 0.1875rem) center;background-size: calc(0.75em + 0.375rem) calc(0.75em + 0.375rem);");
     }else if(!(Number(productStar.value)>=1&&Number(productStar.value)<=10)){
       document.querySelector('.productStar+.invalid-feedback').textContent=`請輸入 1~10 分`;
@@ -211,39 +245,34 @@ function transformNumber(num){
   // Loop over them and prevent submission
   Array.from(forms).forEach(form => {
   form.addEventListener('submit', event => {
+    event.preventDefault();
+    event.stopPropagation();
       if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-          if(document.getElementById('productSelect').value==='請選擇景點地區'){
-              document.querySelector('.productSelect+.invalid-feedback').setAttribute("style","display: block;width: 100%;margin-top: 0.25rem;font-size: 0.875em;color: var(--bs-form-invalid-color);");
-          }
+          form.classList.add('was-validated');
           
           checkStar();
           
-      }else if(document.getElementById('productSelect').value==='請選擇景點地區'){
-          event.preventDefault();
-          event.stopPropagation();
-          document.querySelector('.productSelect+.invalid-feedback').setAttribute("style","display: block;width: 100%;margin-top: 0.25rem;font-size: 0.875em;color: var(--bs-form-invalid-color);");
-          
-          checkStar();
-
       }else if(!(Number(productStar.value)>=1&&Number(productStar.value)<10)){
-        event.preventDefault();
-        event.stopPropagation();
+          form.classList.add('was-validated');  
         
-        checkStar();
+          checkStar();
 
       }else{
-          event.preventDefault();
-          event.stopPropagation();
-          document.querySelector('.productSelect+.invalid-feedback').setAttribute("style","display: none;width: 100%;margin-top: 0.25rem;font-size: 0.875em;color: var(--bs-form-invalid-color);");
-
+          form.classList.remove('was-validated');
+          
           component.addData();
+          
+          productName.value='';
+          productPicUrl.value='';
+          productSelect.value=productSelect[0].value;
+          productPrice.value='';
+          productRemain.value='';
+          productStar.value='';
+          productDescribe.value='';
+          areaSearch.value=areaSearch[0].value;
       }
 
-      form.classList.add('was-validated');
-
-      document.getElementById('productSelect').setAttribute("style","appearance:none ;-moz-appearance:none ;-webkit-appearance:none ;background: url(https://i.imgur.com/8BxTHjt.png) no-repeat 98% #FAFAFA;background-size: 10%;");
+      document.getElementById('productSelect').setAttribute("style","appearance:none ;-moz-appearance:none ;-webkit-appearance:none ;background: url(../assets/images/index/caret-upDown.png) no-repeat 98% #FAFAFA;background-size: 5%;");
   }, false);
   });
 })();
@@ -251,5 +280,3 @@ function transformNumber(num){
 areaSearch.addEventListener('change',e=>{
     component.filter(areaSearch.value);
 });
-
-console.log(123456.5.toLocaleString());
